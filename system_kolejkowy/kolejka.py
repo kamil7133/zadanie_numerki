f79984f0f2d33d9bc5ca8638d636418d4967e840
kamil7133 <piwowarczykkamil23@gmail.com>
2025-01-28 14:20:07 +0100

    zrobilem podstawowe funkcje dla kolejki

--- system_kolejkowy/kolejka.py
+++ system_kolejkowy/kolejka.py
@@ -1,1 +1,18 @@
-git
+class Kolejka:
+    def __init__(self):
+        pass
+
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def front(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
---
+++ system_kolejkowy/system.py
@@ -0,0 +1,6 @@
+from zadanie_numerki.system_kolejkowy import kolejka
+
+
+class System:
+    def __init__(self):
+        pass
